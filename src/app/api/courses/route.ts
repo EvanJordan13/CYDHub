52623d8e424f3cced996ad943c53c8896f75a24b
soph-lin <slin12321@gmail.com>
2025-02-17 21:53:15 -0600

    Create GET endpoint to fetch all courses

---
+++ src/app/api/courses/route.ts
@@ -0,0 +1,12 @@
+import { getAllCourses } from '@/src/lib/query/courses';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const courses = await getAllCourses();
+    return NextResponse.json(courses, { status: 200 });
+  } catch (error) {
+    console.error('[COURSES_GET]', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
