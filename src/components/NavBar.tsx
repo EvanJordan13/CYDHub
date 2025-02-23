9c21d31075dd9749bb03677d033d67384d20f5d3
zlu <ziaxxlu@gmail.com>
2025-02-22 18:29:56 -0600

    Restore stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import { Box, Text, Image, Link, Stack, Button, Flex } from '@chakra-ui/react';
+import { Box, Text, Image, Link, Stack, Button, Flex, Center } from '@chakra-ui/react';
 
-export default function Logo() {
+export default function NavBar() {
   return (
-    <Box py="10px" px="25px" shadow="sm">
-      <Flex direction={'row'}>
-        <Image src="/logo.png" alt="Logo" height="50px" mr="8" />
+    <Box py="5" px="12">
+      <Flex direction={'row'} align="center" justify={'space-between'}>
+        <Text
+          fontFamily="'Poppins', sans-serif"
+          fontSize="3xl"
+          fontWeight="bold"
+          color="#BC3860"
+          whiteSpace="nowrap"
+          mr="4"
+          _hover={{ cursor: 'default' }}
+        >
+          Code Your Dreams
+        </Text>
 
-        <Stack direction={'row'} marginEnd="auto" gap={'10'}>
+        <Stack direction={'row'} gap={'10'}>
           <MenuItem to="/">Dashboard</MenuItem>
           <MenuItem to="/">Classroom</MenuItem>
           <MenuItem to="/">Pricing</MenuItem>
           <MenuItem to="/">Learn</MenuItem>
+
         </Stack>
 
-        <Stack direction={'row'}>
-          <Button rounded={'full'} variant={'outline'} borderWidth={'2px'} borderColor={'black'}>
+        <Stack direction={'row'} gap={'5'}>
+          <Button
+            size={'lg'}
+            px={'7'}
+            rounded={'lg'}
+            variant={'outline'}
+            borderWidth={'1px'}
+            borderBottomWidth={'3px'}
+            borderColor={'#E5E5E5'}
+          >
             Log in
           </Button>
-          <Button rounded={'full'}>Sign up</Button>
+          <Button
+            size={'lg'}
+            px={'7'}
+            rounded={'lg'}
+            bg={'#BC3860'}
+            borderWidth={'1px'}
+            borderBottomWidth={'3px'}
+            borderColor={'#A01B43'}
+          >
+            Sign up
+          </Button>
         </Stack>
       </Flex>
     </Box>
@@ -33,7 +62,7 @@ interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = ({ children, isLast, to = '/', ...rest }) => {
   return (
-    <Link href={to} _focus={{ outline: 'none' }}>
+    <Link href={to} _focus={{ outline: 'none' }} _hover={{ textDecoration: 'none' }}>
       <Text display="block" fontWeight="bold" {...rest}>
         {children}
       </Text>
