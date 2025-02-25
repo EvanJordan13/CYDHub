b6ae3bf0a86108a4f9a4ab04f7db580be3bede7b
soph-lin <slin12321@gmail.com>
2025-02-25 09:17:11 -0600

    Add custom fonts

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { ThemeProvider } from 'next-themes';
 import defaultTheme from '../lib/themes/default';
+import Fonts from '../lib/themes/Fonts';
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={defaultTheme}>
+      <Fonts />
       <ThemeProvider attribute="class" disableTransitionOnChange>
         {props.children}
       </ThemeProvider>
---
+++ src/lib/themes/Fonts.tsx
@@ -0,0 +1,18 @@
+// Fonts.tsx
+'use client';
+
+import { Global } from '@emotion/react';
+
+const Fonts = () => (
+  <Global
+    styles={`
+      @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+      body {
+        font-family: 'Poppins', sans-serif;
+      }
+    `}
+  />
+);
+
+export default Fonts;
