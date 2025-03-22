dd03ea5435c20578e36a94f0976d5a9c8993ff33
soph-lin <slin12321@gmail.com>
2025-03-22 14:19:54 -0500

    Create announcement card

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,9 +1,10 @@
 import TextInput from '../../components/TextInput';
 import ProgramCard from '../../components/ProgramCard';
-import { Box, Heading, Flex } from '@chakra-ui/react';
+import { Box, Heading, Flex, VStack } from '@chakra-ui/react';
 import { Program } from '@prisma/client';
 import { User, Calendar, Award } from 'lucide-react';
 import Button from '@/src/components/Button';
+import AnnouncementCard, { AnnouncementCardProps } from '@/src/components/AnnouncementCard';
 
 export default function DevPage() {
   // Mock course data
@@ -18,9 +19,42 @@ export default function DevPage() {
     teacherId: null,
   };
 
+  const mockAnnouncements: AnnouncementCardProps[] = [
+    {
+      subject: 'Portfolio Graded',
+      title: 'HTML Portfolio Graded',
+      message: 'Your HTML Portfolio assignment has been graded.',
+      name: 'Penguin Pants',
+      avatarUrl: 'https://i.pinimg.com/736x/f3/d5/15/f3d515f1d9105545a5954fac24f27a06.jpg',
+      createdAt: new Date('2025-01-25 06:56:14.46'),
+      link: '/programs/1/announcements/1',
+    },
+    {
+      subject: 'Course Update',
+      title: 'New Module Posted',
+      message: 'New module "CSS Fundamentals" has been released.',
+      name: 'Jane Smith',
+      createdAt: new Date('2025-01-14 06:56:14.46'),
+      link: '/modules/2',
+    },
+  ];
+
   return (
     <Box p={8} bg={'white'}>
       <Heading mb={6}>Development Page</Heading>
+
+      {/* Announcements Section */}
+      <Box my={8} width={'1136px'}>
+        <Heading as="h2" size="lg" mb={4}>
+          Announcements
+        </Heading>
+        <VStack gap={'32px'} align="stretch">
+          {mockAnnouncements.map((announcement, index) => (
+            <AnnouncementCard key={index} {...announcement} />
+          ))}
+        </VStack>
+      </Box>
+
       <TextInput label="Date of Birth" width={10} icon={<Calendar />} />
       <br />
       <TextInput label="Name" width={18.75} icon={<User />} />
