4b24e487074c50bb55071b494fd0297cf342141c
SaketR3 <saketr3@illinois.edu>
2025-03-24 18:42:54 -0500

    Fix due date rendering

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -246,7 +246,7 @@ export default function DevPage() {
                     <Text fontWeight="bold">{assignment.title}</Text>
                     <Text>Module: {assignment.moduleTitle}</Text>
                     <Text>Description: {assignment.description}</Text>
-                    <Text>Due Date: {assignment.dueDate.toString()}</Text>
+                    (assignment.dueDate && <Text>Due Date: {assignment.dueDate.toString()}</Text>)
                     {assignment.fileUrl && (
                       <Text>
                         File:{' '}
