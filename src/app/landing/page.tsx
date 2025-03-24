700b2fcb225f88f8deb9f708b64cfa396b30573f
Vivek Turakhia <vturakhia55@gmail.com>
2025-03-24 18:55:54 -0500

    fix formatting

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,54 +1,71 @@
-import { Box, HStack, VStack, Text, Image } from "@chakra-ui/react";
-import InformationCard from "@/src/components/InformationCard";
-import Button from "@/src/components/Button";
+import { Box, HStack, VStack, Text, Image } from '@chakra-ui/react';
+import InformationCard from '@/src/components/InformationCard';
+import Button from '@/src/components/Button';
 
 export default function LandingPage() {
-    return (
-        <VStack w="100vw" minH="100vh" bg="white" p={8} position="relative" overflow="hidden" zIndex={-3}>
-            <Box
-                position="absolute"
-                bottom={-900}
-                left="50%"
-                transform="translateX(-50%)"
-                width="200vw" 
-                height="170vw" 
-                bg={"Flamingo"} 
-                borderRadius="50%"  
-                zIndex={-2} 
-            />
-            <Box
-                position="absolute"
-                bottom={-1420}  
-                left="100%"
-                transform="translateX(-100%)"
-                width="100vw"  
-                height="150vw"
-                bg={"White"} 
-                zIndex={-1}  
-            />              
-            <VStack>
-                <VStack paddingY="100px">
-                <Text fontSize="18px" color="Slate" fontFamily={"Poppins"} fontWeight={700}>Welcome to the Student Hub</Text>
-                <Text fontSize="72px" color="Flamingo" fontWeight={700}>Code Your Dreams</Text>
-                <HStack paddingY="30px">
-                    <Button type="secondary" text="Learn More" width="178px" height="56px" pageColor="flamingo"/>
-                    <Button type="primary" text="Get Started" width="178px" height="56px" pageColor="flamingo"/>
-                </HStack>
-                </VStack>
-                <Image src="/temp-screen.png" width="931px" height="596px" />
-                <HStack marginTop="130px" gap="24px" marginBottom="100px">
-                    <InformationCard  title= "Centralized Learning Dashboard" description= "Easily access all your courses, assignments, and learning materials in one place."  />
-                    <InformationCard  title= "Centralized Learning Dashboard" description= "Easily access all your courses, assignments, and learning materials in one place."  />
-                    <InformationCard  title= "Centralized Learning Dashboard" description= "Easily access all your courses, assignments, and learning materials in one place."  />
-                </HStack>
-                <HStack gap={60}>
-                    <Image src="/class-path.png" width="663px" height="620px" />
-                    <VStack width="353px" height="221px">
-                        <Text fontSize="48px" color="Flamingo" fontFamily="Poppins" fontWeight={700}>Gamified Learning</Text>
-                        <Text fontSize="18px" color="Slate" fontWeight={500} fontFamily="Poppins">Turning challenges into rewarding experiences makes coding fun and efficient.</Text>
-                    </VStack>
-                </HStack>
-            </VStack>
+  return (
+    <VStack w="100vw" minH="100vh" bg="white" p={8} position="relative" overflow="hidden" zIndex={-3}>
+      <Box
+        position="absolute"
+        bottom={-900}
+        left="50%"
+        transform="translateX(-50%)"
+        width="200vw"
+        height="170vw"
+        bg={'Flamingo'}
+        borderRadius="50%"
+        zIndex={-2}
+      />
+      <Box
+        position="absolute"
+        bottom={-1420}
+        left="100%"
+        transform="translateX(-100%)"
+        width="100vw"
+        height="150vw"
+        bg={'White'}
+        zIndex={-1}
+      />
+      <VStack>
+        <VStack paddingY="100px">
+          <Text fontSize="18px" color="Slate" fontFamily={'Poppins'} fontWeight={700}>
+            Welcome to the Student Hub
+          </Text>
+          <Text fontSize="72px" color="Flamingo" fontWeight={700}>
+            Code Your Dreams
+          </Text>
+          <HStack paddingY="30px">
+            <Button type="secondary" text="Learn More" width="178px" height="56px" pageColor="flamingo" />
+            <Button type="primary" text="Get Started" width="178px" height="56px" pageColor="flamingo" />
+          </HStack>
         </VStack>
-    );
+        <Image src="/temp-screen.png" width="931px" height="596px" />
+        <HStack marginTop="130px" gap="24px" marginBottom="100px">
+          <InformationCard
+            title="Centralized Learning Dashboard"
+            description="Easily access all your courses, assignments, and learning materials in one place."
+          />
+          <InformationCard
+            title="Centralized Learning Dashboard"
+            description="Easily access all your courses, assignments, and learning materials in one place."
+          />
+          <InformationCard
+            title="Centralized Learning Dashboard"
+            description="Easily access all your courses, assignments, and learning materials in one place."
+          />
+        </HStack>
+        <HStack gap={60}>
+          <Image src="/class-path.png" width="663px" height="620px" />
+          <VStack width="353px" height="221px">
+            <Text fontSize="48px" color="Flamingo" fontFamily="Poppins" fontWeight={700}>
+              Gamified Learning
+            </Text>
+            <Text fontSize="18px" color="Slate" fontWeight={500} fontFamily="Poppins">
+              Turning challenges into rewarding experiences makes coding fun and efficient.
+            </Text>
+          </VStack>
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 }
--- src/components/InformationCard.tsx
+++ src/components/InformationCard.tsx
@@ -9,19 +9,23 @@ interface InformationCardProps {
 export default function InformationCard({ title, description }: InformationCardProps) {
   return (
     <Box
-        height={266}
-        width={408}
-        bg={'white'}
-        borderWidth={'2px'}
-        borderColor={'lightgray'}
-        borderRadius={'2xl'}
-        padding="28px"
+      height={266}
+      width={408}
+      bg={'white'}
+      borderWidth={'2px'}
+      borderColor={'lightgray'}
+      borderRadius={'2xl'}
+      padding="28px"
     >
-    <VStack alignItems="left">
+      <VStack alignItems="left">
         <Image src="/information-card-icon.png" width="32px" height="32px" />
-        <Text fontWeight={700} fontSize="32px" fontFamily={"Poppins"} color="Slate">{title}</Text>
-        <Text fontWeight={500} fontSize="16px" fontFamily={"Poppins"} color="Slate">{description}</Text>
-    </VStack>
+        <Text fontWeight={700} fontSize="32px" fontFamily={'Poppins'} color="Slate">
+          {title}
+        </Text>
+        <Text fontWeight={500} fontSize="16px" fontFamily={'Poppins'} color="Slate">
+          {description}
+        </Text>
+      </VStack>
     </Box>
   );
 }
