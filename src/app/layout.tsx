ff574e4affd76670fcdcbe7b4a5d7c71c9b75882
zlu <ziaxxlu@gmail.com>
2025-03-27 09:50:27 -0500

    Removed layout from PR

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import Provider from './providers';
+import type { Metadata } from 'next';
+import { siteConfig } from '@/src/config/site';
+import Footer from '@/src/components/Footer';
+import NavBar from '@/src/components/NavBar';
+import Fonts from '../lib/themes/Fonts';
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <Provider>
+          <main>{children}</main>
+          <NavBar />
+          <Footer />
+        </Provider>
+      </body>
+    </html>
+  );
+}
