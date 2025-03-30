857febd1d6e745703339c4aa3ca2b642f9eae9fe
soph-lin <slin12321@gmail.com>
2025-03-29 23:09:29 -0500

    Format

--- src/lib/query/users.ts
+++ src/lib/query/users.ts
@@ -1,24 +1,24 @@
-"use server";
+'use server';
 
 import prisma from '@/src/lib/postgres/db';
 import { User } from '@prisma/client';
 
 export async function getUserById(userId: number): Promise<User> {
-    if (userId === null) {
-        throw new Error(`ID provided is null`);
-    }
+  if (userId === null) {
+    throw new Error(`ID provided is null`);
+  }
 
-    const user = await prisma.user.findUnique({
-        where: {
-        id: userId,
-        },
-    });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
 
-    if (!user) {
-        throw new Error(`No user found with ID ${userId}`);
-    }
+  if (!user) {
+    throw new Error(`No user found with ID ${userId}`);
+  }
 
-    console.log("found user", user);
+  console.log('found user', user);
 
-    return user;
+  return user;
 }
