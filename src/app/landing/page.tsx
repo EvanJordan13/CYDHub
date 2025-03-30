0819e6859799960963ff86ef4fba276b25b30737
Vivek Turakhia <vturakhia55@gmail.com>
2025-03-29 19:26:21 -0500

    edited for pr comments

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -5,33 +5,12 @@ import Button from '@/src/components/Button';
 export default function LandingPage() {
   return (
     <VStack w="100vw" minH="100vh" bg="white" p={8} position="relative" overflow="hidden" zIndex={-3}>
-      <Box
-        position="absolute"
-        bottom={-900}
-        left="50%"
-        transform="translateX(-50%)"
-        width="200vw"
-        height="170vw"
-        bg={'Flamingo'}
-        borderRadius="50%"
-        zIndex={-2}
-      />
-      <Box
-        position="absolute"
-        bottom={-1420}
-        left="100%"
-        transform="translateX(-100%)"
-        width="100vw"
-        height="150vw"
-        bg={'White'}
-        zIndex={-1}
-      />
-      <VStack>
+      <VStack w="100%" h="100%">
         <VStack paddingY="100px">
-          <Text fontSize="18px" color="Slate" fontFamily={'Poppins'} fontWeight={700}>
+          <Text fontSize="clamp(14px, 1.18vw, 18px)" color="Slate" fontFamily={'Poppins'} fontWeight={700}>
             Welcome to the Student Hub
           </Text>
-          <Text fontSize="72px" color="Flamingo" fontWeight={700}>
+          <Text fontSize="clamp(36px, 4.7vw, 72px)" color="Flamingo" fontWeight={700}>
             Code Your Dreams
           </Text>
           <HStack paddingY="30px">
@@ -39,8 +18,22 @@ export default function LandingPage() {
             <Button type="primary" text="Get Started" width="178px" height="56px" pageColor="flamingo" />
           </HStack>
         </VStack>
-        <Image src="/temp-screen.png" width="931px" height="596px" />
-        <HStack marginTop="130px" gap="24px" marginBottom="100px">
+        <Box position="relative" width="100vw" mx="auto" display="flex" justifyContent="center">
+          <Box
+            position="absolute"
+            top="100px"
+            left="-50%"
+            width="200%"
+            paddingBottom="100%"
+            bg="Flamingo"
+            borderRadius="50%"
+            zIndex={-1}
+          />
+
+          <Image src="/temp-screen.png" width="64vw" height="auto" zIndex={1} position="relative" />
+        </Box>
+
+        <HStack marginTop="130px" gap="4vw" marginBottom="100px">
           <InformationCard
             title="Centralized Learning Dashboard"
             description="Easily access all your courses, assignments, and learning materials in one place."
@@ -52,15 +45,25 @@ export default function LandingPage() {
           <InformationCard
             title="Centralized Learning Dashboard"
             description="Easily access all your courses, assignments, and learning materials in one place."
+          />
+          <Box
+            position="absolute"
+            left="-50%"
+            transform="translateY(50%)"
+            width="200%"
+            paddingBottom="100%"
+            bg="white"
+            zIndex={-1}
           />
         </HStack>
+
         <HStack gap={60}>
-          <Image src="/class-path.png" width="663px" height="620px" />
+          <Image src="/class-path.png" width="46vw" height="auto" />
           <VStack width="353px" height="221px">
-            <Text fontSize="48px" color="Flamingo" fontFamily="Poppins" fontWeight={700}>
+            <Text fontSize="clamp(24px, 3.1vw, 48px)" color="Flamingo" fontFamily="Poppins" fontWeight={700}>
               Gamified Learning
             </Text>
-            <Text fontSize="18px" color="Slate" fontWeight={500} fontFamily="Poppins">
+            <Text fontSize="clamp(14px, 1.18vw, 18px)" color="Slate" fontWeight={500} fontFamily="Poppins">
               Turning challenges into rewarding experiences makes coding fun and efficient.
             </Text>
           </VStack>
