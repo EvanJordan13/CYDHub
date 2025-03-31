2fc50aef71678cf8b76dca3c7aadc1ca8100daa4
EvanJ <evanj3@illinois.edu>
2025-03-31 16:26:33 -0500

    file cleanup

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -61,7 +61,6 @@ export default function DevPage() {
     updatedAt: new Date(),
     type: 'IN_PERSON',
     teacherId: 1,
-    subject: 'Date Science',
   };
 
   const testFetchAllPrograms = async () => {
