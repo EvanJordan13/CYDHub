4e57070b94ff5e9a3a9af89627edcbd7024bb62b
soph-lin <slin12321@gmail.com>
2025-05-04 13:38:54 -0500

    Format

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,8 @@
 import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 
-
 export default withMiddlewareAuthRequired({
   returnTo: '/api/auth/login',
 });
-
 
 export const config = {
   matcher: [
