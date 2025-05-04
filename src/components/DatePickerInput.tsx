6c8f9044a4b6cdcfade7c7b97224af48de245749
EvanJ <evanj3@illinois.edu>
2025-05-04 04:57:19 -0500

    code cleanup

--- src/components/DatePickerInput.tsx
+++ src/components/DatePickerInput.tsx
@@ -11,7 +11,7 @@ interface DatePickerInputProps {
   helperText: string;
   isRequired?: boolean;
   showIcon?: boolean;
-  height?: string | number;
+  height?: number;
   val?: string | null;
   onChange?: (value: string) => void;
 }
@@ -21,7 +21,7 @@ const DatePickerInput: React.FC<DatePickerInputProps> = ({
   helperText,
   isRequired = false,
   showIcon = true,
-  height = '12',
+  height = 12,
   val,
   onChange,
   ...rest
